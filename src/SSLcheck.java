76422d4c88a4f8c5b3655436f354cc9bf45bd860
Syed Rehman G <syedrehman9617@gmail.com>
2025-06-21 19:37:33 +0530

    Add SSLcheck Java files

---
+++ src/SSLcheck.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+
+public class SSLcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeOptions option = new ChromeOptions();
+		option.setAcceptInsecureCerts(true);
+		
+		WebDriver driver = new ChromeDriver(option);
+		driver.get("https://expired.badssl.com/");
+		driver.getTitle();
+
+	}
+
+}
